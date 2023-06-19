5df47c7ee615bd2084d214eb89289ad3d0002d0b
npc299792 <137055935+npc299792@users.noreply.github.com>
2023-06-19 12:03:09 +0200

    Add files via upload

---
+++ JPS.js
@@ -0,0 +1,104 @@
+﻿
+const JPS = { TYPES: {
+	PARTICLE: {x:0, y:0, z:0, s:1, r:0, sx:1, sy:1, rx:0, ry:0, age:0}
+}}
+
+// #################################################################################################################################
+
+JPS.TOOLS = {
+	RND: function(v = 0, r, i) {
+		switch(v.constructor) {
+			case Object: return JPS.TOOLS.RNDRGB(v)
+			case Array:  i=v[2]||i; r=v[1]; v=v[0]
+		}
+		if (r) v += Math.random()*2*r - r
+		return i && ~~v || v
+	},
+
+	RGB: function(r = 0, g = 0, b = 0, a = 1) {
+		r = ((1<<24) + (r<<16) + (g<<8) + b).toString(16).slice(1)
+		a = (a*255 < 10 && '0' || '') + (~~(a*255)).toString(16)
+		return "#" + (r + a).toUpperCase()
+	},
+
+	RNDRGB: function(c) {
+		return JPS.TOOLS.RGB(JPS.TOOLS.RND(c.r,0,1), JPS.TOOLS.RND(c.g,0,1), JPS.TOOLS.RND(c.b,0,1), JPS.TOOLS.RND(c.a))
+	}
+}
+
+// #################################################################################################################################
+
+JPS.SYSTEM = class {
+	x = 0; y = 0; s = 1; time = 0; skip = 0; speed = 1
+	constructor() { this.clear(1,1); this.addModifier(...arguments) }
+
+	spawn(c = 1, ctx) {
+		var p = {...JPS.TYPES.PARTICLE}
+		this.modifiers.init.forEach(m => m.type == p.type && m.init(p,ctx,this))
+		this.particles.unshift(p)
+		return (c > 1) && this.spawn(--c,ctx) || p
+	}
+
+	delete(p, ctx) {
+		p = this.particles.splice(p,1)
+		this.modifiers.exit.forEach(m => m.type == p.type && m.exit(p,ctx,this))
+	}
+	
+	clear(p, m = 0) {
+		if (p) this.particles = []
+		if (m) this.modifiers = {prev:[], init:[], render:[], exit:[], post:[]}
+	}
+	
+	// #############################################################################################################################
+	
+	addModifier() {
+		for (var c in this.modifiers) for (var m of arguments) if (m[c]) this.modifiers[c].push(m)
+		for (m of arguments) if (m.init) for (c of this.particles) m.init(c,0,this)
+	}
+
+	removeModifier() {
+		for (var c in this.modifiers) for (var m of arguments) if (m[c]) {
+			m = this.modifiers[c].findIndex(s => m === s)
+			if (m >= 0) this.modifiers[c].splice(m,1)
+		}
+	}
+	
+	removeParticle() {
+		for (var p of arguments) {
+			if (p.constructor !== Number) p = this.particles.findIndex(q => p === q)
+			if (p >= 0) this.delete(p)
+		}
+	}
+
+	// #############################################################################################################################
+	
+	render(ctx, time = 16.66) {
+		this.time += time; this.delta = time; this.skip = 0
+		var stime = this.speed*time/1000
+		
+		this.modifiers.prev.forEach(m => m.prev(ctx,stime,this))
+		ctx?.save()
+
+		for (var i = 0; i < this.particles.length; i++) {
+			var p = this.particles[i]
+			p.age += stime*1000
+			
+			if (p.life && p.age > p.life) 
+				this.delete(i--)
+			else {
+				if (ctx) {
+					ctx.setTransform(p.s*p.sx, p.s*p.rx, p.s*p.ry, p.s*p.sy, this.s*p.x+this.x, this.s*p.y+this.y)
+					ctx.scale(this.s, this.s)
+					p.r && ctx.rotate(p.r)
+				}
+				for (var m of this.modifiers.render) {
+					m = m.type == p.type && m.render(p,ctx,stime,this,i)
+					if(m) { m == 1 && this.delete(i--); this.skip++; break }
+				}
+			}
+		}
+
+		ctx?.restore()
+		this.modifiers.post.forEach(m => m.post(ctx,stime,this))
+	}
+}
---
+++ JPS_LIB.js
@@ -0,0 +1,232 @@
+﻿
+JPS.LIB ??= {}
+
+// #################################################################################################################################
+
+JPS.TYPES.BASE		= {s:1, r:1, life:1, type:1}
+JPS.TYPES.COLOR		= {r:[128,64], g:[128,64], b:[128,64], a:[0.6,0.4]}
+JPS.TYPES.GRADIENT	= [{i:0.3, r:[200,50], g:[200,50], b:[200,50], a:1}, {i:1, r:0, g:0, b:0, a:0}]
+
+JPS.TYPES.RECT		= {w:[10,10], h:[10,10], color:JPS.TYPES.COLOR, method:'fillRect'}
+JPS.TYPES.CIRCLE	= {radius:[10,10], color:JPS.TYPES.COLOR}
+JPS.TYPES.SPHERE	= {radius:[10,10], gradient:JPS.TYPES.GRADIENT}
+JPS.TYPES.TEXT		= {size:[20,10], color:JPS.TYPES.COLOR, font:'Arial', method:'fillText', text:['A','B','C']}
+JPS.TYPES.IMAGE		= {x:0, y:0, s:[1,1]}
+
+JPS.TYPES.EMITTER	= {x:0, y:0, z:0, w:100, h:100, d:0, spawn:0, rate:0}
+JPS.TYPES.ANIMATE	= {id:'v', data:{x:[0,100], y:[0,100]}}
+JPS.TYPES.WALL		= {id:'v', x:0, y:0, w:100, h:100, size:1, bounce:0.6}
+JPS.TYPES.INFO		= {x:5, y:20, size:15, color:'grey', font:'Arial', method:'fillText', text1:'fps: %%fps  /  particles: %%p'}
+JPS.TYPES.CAMERA	= {x:0, y:0, z:-1000, depth:2000, fov:Math.PI/2, center:{x:window.innerWidth/2,y:window.innerHeight/2}, mode:2}
+
+JPS.TYPES.NEWTON	= {id:'v', mass:1, const:1.1}
+JPS.TYPES.BOX		= {id:'v', x:0, y:0, w:500, h:500}
+
+// #################################################################################################################################
+
+JPS.TOOLS.RNDASN = function(r, ...a) {
+	for (o of a) for (i in o) r[i] = JPS.TOOLS.RND(o[i])
+	return r
+}
+
+JPS.TOOLS.RNDBASE = function(a, b) {
+	for (i in JPS.TYPES.BASE) if (b[i]) a[i] = JPS.TOOLS.RND(b[i])
+}
+
+JPS.TOOLS.PRINT = function(ctx, t, h=20, x=0, y=0, f, c, a, b, m='fillText') {
+	if (f) ctx.font = h + 'px ' + f
+	if (c) ctx.fillStyle = c
+	if (a) ctx.textAlign = a
+	if (b) ctx.textBaseline = b
+	if (t) ctx[m](t, x, y)
+}
+
+JPS.TOOLS.INRECT = (x,y,a,b,c,d) => x > a && x < c && y > b && y < d
+JPS.TOOLS.INBOUND = (x,y,w,h,a,b,c,d) => x < a+c && x+w > a && y < b+d && y+h > b
+JPS.TOOLS.INTERSECT = (x,y,u,v,a,b,c,d) => {
+	l = u * d - v * c
+	t = -(x * d - y * c - a * d + b * c) / l
+	if (t < 0 || t > 1) return false
+	s = -(x * v - y * u + u * b - v * a) / l
+	return s > 0 && s < 1
+}
+
+// #################################################################################################################################
+
+
+JPS.LIB.RECTANGLE = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.RECT, data)
+	}
+	init(p) {
+		JPS.TOOLS.RNDASN(p, this)
+	}
+	render(p, ctx) {
+		if (!ctx) return
+		ctx.fillStyle = p.color
+		ctx[this.method](-p.w/2, -p.h/2, p.w, p.h)
+	}
+}
+
+
+JPS.LIB.CIRCLE = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.CIRCLE, data)
+	}
+	init(p) {
+		JPS.TOOLS.RNDASN(p, this)
+	}
+	render(p, ctx) {
+		if (!ctx) return
+		ctx.fillStyle = p.color
+		ctx.beginPath(); ctx.arc(0, 0, p.radius, 0, 360); ctx.fill()
+	}
+}
+
+
+JPS.LIB.SPHERE = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.SPHERE, data)
+	}
+	init(p, ctx) {
+		JPS.TOOLS.RNDBASE(p, this)
+		p.radius = JPS.TOOLS.RND(this.radius)
+		p.gradient = ctx.createRadialGradient(0,0,0,0,0,p.radius)
+		this.gradient.forEach(c => p.gradient.addColorStop(c.i, JPS.TOOLS.RND(c)))
+	}
+	render(p, ctx) {
+		if (!ctx) return
+		ctx.fillStyle = p.gradient
+		ctx.beginPath(); ctx.arc(0, 0, p.radius, 0, 360); ctx.fill()
+	}
+}
+
+
+JPS.LIB.IMAGE = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.IMAGE, data)
+		this.image = new Image()
+		this.image.src = data.src
+	}
+	init(p) {
+		JPS.TOOLS.RNDBASE(p, this)
+	}
+	render(p, ctx) {
+		ctx?.drawImage(this.image, -this.image.width/2, -this.image.height/2)
+	}
+}
+
+
+JPS.LIB.TEXT = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.TEXT, data)
+	}
+	init(p) {
+		JPS.TOOLS.RNDBASE(p, this)
+		p.h		= JPS.TOOLS.RND(this.size,0,1)
+		p.color	= JPS.TOOLS.RNDRGB(this.color)
+		p.text  = this.text[~~(Math.random()*this.text.length)]
+	}
+	render(p, ctx) {
+		ctx && JPS.TOOLS.PRINT(ctx, p.text, p.h, 0, p.h, this.font, p.color, 0,0, this.method)
+	}
+}
+
+// #################################################################################################################################
+
+
+JPS.LIB.EMITTER = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.EMITTER, data)
+	}
+	post(ctx, time, sys) {
+		this.spawn += time * this.rate
+		while (this.spawn >= 1) {
+			var p = sys.spawn(1, ctx)
+			JPS.TOOLS.RNDBASE(p, this)
+			if (Array.isArray(p.type)) p.type = p.type[~~(Math.random()*p.type.length)]
+			p.x = JPS.TOOLS.RND(this.x, this.w/2)
+			p.y = JPS.TOOLS.RND(this.y, this.h/2)
+			p.z = JPS.TOOLS.RND(this.z, this.d/2)
+			this.spawn--
+		}
+	}
+}
+
+
+JPS.LIB.ANIMATE = class {
+	constructor(data, id) {
+		Object.assign(this, JPS.TYPES.ANIMATE, data)
+		this.data = data || this.data
+	}
+	init(p) {
+		for (var e in this.data) p[this.id+e] = JPS.TOOLS.RND(this.data[e])
+	}
+	render(p, ctx, time, sys) {
+		for (var e in this.data) p[e] += p[this.id+e] * time
+	}
+}
+
+
+JPS.LIB.CAMERA = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.CAMERA, data)
+	}
+	render(p, ctx) {
+		if (!ctx) return
+		
+		var w = 1 - this.depth / (p.z - this.z)
+		if (w >= 0) return this.mode
+		
+		p.r && ctx.rotate(-p.r)
+		ctx.translate(
+			(this.x - p.x * this.fov) * w + this.center.x - this.x, 
+			(this.y - p.y * this.fov) * w + this.center.y - this.y
+		)
+		ctx.scale(-w,-w)
+		p.r && ctx.rotate(p.r)
+	}
+}
+
+
+JPS.LIB.WALL = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.WALL, data)
+		this.post = this.color && function(ctx) { if (ctx) {
+			ctx.strokeStyle = this.color; ctx.lineWidth = this.size
+			ctx.beginPath(); ctx.moveTo(this.x,this.y); ctx.lineTo(this.x+this.w,this.y+this.h); ctx.stroke()
+		}}
+	}
+	render(p, ctx, time, sys) {
+		var vx = p[this.id+'x'], vy = p[this.id+'y'], len = Math.sqrt(vx**2 + vy**2)
+		var ab = p.s * (p.radius || p.h || p.w || 10) + time*len + this.size
+
+		if (JPS.TOOLS.INTERSECT(this.x,this.y, this.w,this.h, p.x,p.y, p.sx*ab*(vx/len), p.sy*ab*(vy/len))) {
+			ab = Math.atan2(this.w,this.h)*2 + Math.PI/2 + Math.atan2(vy,vx)
+			len *= -JPS.TOOLS.RND(this.bounce)
+			p[this.id+'x'] = Math.sin(ab)*len
+			p[this.id+'y'] = Math.cos(ab)*len
+			this.callback?.(p,sys,this)
+		}
+	}
+}
+
+
+JPS.LIB.INFO = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.INFO, data)
+		this.render = this.text2 && function(ctx) { if (ctx) {
+			var txt = this.text2
+			for (i in p) txt = txt.replace('%%'+i, p[i])
+			JPS.TOOLS.PRINT(
+				ctx, txt, this.size, this.offset?.x || 0, this.offset?.y || 0, 
+				this.font, this.color, this.align, this.base, this.method
+			)
+		}}
+	}
+	post(ctx, time, sys) {
+		var txt = this.text1.replace('%%p', sys.particles.length).replace('%%fps', Math.round(1000/sys.delta)).replace('%%ms', time*1000)
+		txt = txt.replace('%%time', ~~sys.time).replace('%%speed', sys.speed).replace('%%skip', sys.skip)
+		JPS.TOOLS.PRINT(ctx, txt, this.size, this.x, this.y, this.font, this.color, this.align, this.base, this.method)
+	}
+}
