e6a7135c377c5b52f6cd12f7715f3a40a1d316cb
npc299792 <137055935+npc299792@users.noreply.github.com>
2023-09-19 13:29:33 +0200

    Add files via upload

--- JPS.js
+++ JPS.js
@@ -1,48 +1,59 @@
-﻿// JPS - Javascript Particle System  |  v0.9  |  npc299792  |  MIT Licence
+﻿// JPS - Javascript Particle System  |  v0.98  |  npc299792  |  MIT Licence
 
 const JPS = { TYPES: {
-	PARTICLE: {x:0, y:0, z:0, s:1, r:0, sx:1, sy:1, rx:0, ry:0, age:0}
+	PARTICLE: {x:0, y:0, s:1, r:0, sx:1, sy:1, rx:0, ry:0, age:0}
 }}
 
 // #################################################################################################################################
 
 JPS.TOOLS = {
 	RND: function(v = 0, r, i) {
-		switch(v.constructor) {
-			case Object: return JPS.TOOLS.RNDRGB(v)
-			case Array:  i=v[2]||i; r=v[1]; v=v[0]
-		}
+		if (Array.isArray(v)) {
+			if (typeof v[0] == 'number') { i=v[2]||i; r=v[1]; v=v[0]||0 }
+			else return JPS.TOOLS.RND(v[~~(Math.random()*v.length)])
+		} 
+		if (typeof v == 'object') return JPS.TOOLS.RNDRGB(v)
+		if (typeof v == 'string') return v
 		if (r) v += Math.random()*2*r - r
 		return i && ~~v || v
 	},
 
 	RGB: function(r = 0, g = 0, b = 0, a = 1) {
 		r = ((1<<24) + (r<<16) + (g<<8) + b).toString(16).slice(1)
-		a = (a*255 < 10 && '0' || '') + (~~(a*255)).toString(16)
+		a = a <= 0 && '00' || (a*255 < 10 && '0' || '') + (~~(a*255)).toString(16)
 		return "#" + (r + a).toUpperCase()
 	},
 
 	RNDRGB: function(c) {
-		return JPS.TOOLS.RGB(JPS.TOOLS.RND(c.r,0,1), JPS.TOOLS.RND(c.g,0,1), JPS.TOOLS.RND(c.b,0,1), JPS.TOOLS.RND(c.a))
+		return JPS.TOOLS.RGB(JPS.TOOLS.RND(c.r||0,0,1), JPS.TOOLS.RND(c.g||0,0,1), JPS.TOOLS.RND(c.b||0,0,1), JPS.TOOLS.RND(c.a||1))
+	},
+
+	TYP: function(m, p) {
+		if (!m || !p || m == p) return true
+		if (Array.isArray(m)) return m.includes(p)
 	}
 }
 
 // #################################################################################################################################
 
 JPS.SYSTEM = class {
-	x = 0; y = 0; s = 1; time = 0; skip = 0; speed = 1
+	x = 0; y = 0; s = 1; time = 0; speed = 1; skip = 0
 	constructor() { this.clear(1,1); this.addModifier(...arguments) }
 
-	spawn(c = 1, ctx) {
-		var p = {...JPS.TYPES.PARTICLE}
-		this.modifiers.init.forEach(m => m.type == p.type && m.init(p,ctx,this))
-		this.particles.unshift(p)
-		return (c > 1) && this.spawn(--c,ctx) || p
+	spawn(c = 1, ctx, data) {
+		var list = []
+		while (c-- >= 1) {
+			if (this.max && this.max <= this.particles.length) break
+			var p = {...JPS.TYPES.PARTICLE, ...data}
+			this.modifiers.init.forEach(m => JPS.TOOLS.TYP(m.type,p.type) && m.init(p,ctx,this))
+			this.particles.unshift(p); list.push(p)
+		}
+		return list.length == 1 && list[0] || list
 	}
 
 	delete(p, ctx) {
 		p = this.particles.splice(p,1)
-		this.modifiers.exit.forEach(m => m.type == p.type && m.exit(p,ctx,this))
+		this.modifiers.exit.forEach(m => JPS.TOOLS.TYP(m.type,p.type) && m.exit(p,ctx,this))
 	}
 	
 	clear(p, m) {
@@ -53,21 +64,32 @@ JPS.SYSTEM = class {
 	// #############################################################################################################################
 	
 	addModifier() {
-		for (var c in this.modifiers) for (var m of arguments) if (m[c]) this.modifiers[c].push(m)
-		for (m of arguments) if (m.init) for (c of this.particles) m.init(c,0,this)
+		for (var m of arguments) {
+			if (Array.isArray(m)) { this.addModifier(...m); continue }
+			if (m.constructor.name == 'CanvasRenderingContext2D') { var ctx = m; break }
+			m.add?.(this)
+			for (var c in this.modifiers) if (m[c]) this.modifiers[c].push(m)
+			if (m.init) for (var p of this.particles) m.init(p,ctx,this)
+		}
 	}
 
 	removeModifier() {
-		for (var c in this.modifiers) for (var m of arguments) if (m[c]) {
-			m = this.modifiers[c].findIndex(s => m === s)
-			if (m >= 0) this.modifiers[c].splice(m,1)
+		for (var m of arguments) {
+			if (Array.isArray(m)) { this.removeModifier(...m); continue }
+			for (var c in this.modifiers) if (m[c]) {
+				m = this.modifiers[c].findIndex(s => m === s)
+				if (m >= 0) this.modifiers[c].splice(m,1)
+			}
+			m.remove?.(this)
 		}
 	}
 	
 	removeParticle() {
 		for (var p of arguments) {
-			if (p.constructor !== Number) p = this.particles.findIndex(q => p === q)
-			if (p >= 0) this.delete(p)
+			if (Array.isArray(p)) { this.removeParticle(...p); continue }
+			if (p.constructor.name == 'CanvasRenderingContext2D') { var ctx = p; break }
+			if (typeof p !== Number) p = this.particles.findIndex(q => p === q)
+			if (p >= 0) this.delete(p,ctx)
 		}
 	}
 
@@ -78,30 +100,27 @@ JPS.SYSTEM = class {
 		this.time += stime*1000; this.delta = time; this.skip = 0
 
 		this.modifiers.prev.forEach(m => m.prev(ctx,stime,this))
-		this.rendertime = performance.now()
+		var ctime = performance.now()
 		ctx?.save()
 
 		for (var i = 0; i < this.particles.length; i++) {
 			var p = this.particles[i]
 			p.age += stime*1000
+			if (p.life && p.age > p.life) { this.delete(i--); continue }
 			
-			if (p.life && p.age > p.life) 
-				this.delete(i--)
-			else {
-				if (ctx) {
-					ctx.setTransform(p.s*p.sx, p.s*p.rx, p.s*p.ry, p.s*p.sy, this.s*p.x+this.x, this.s*p.y+this.y)
-					ctx.scale(this.s, this.s)
-					p.r && ctx.rotate(p.r)
-				}
-				for (var m of this.modifiers.render) {
-					m = m.type == p.type && m.render(p,ctx,stime,this,i)
-					if(m) { m == 1 && this.delete(i--); this.skip++; break }
-				}
+			if (ctx) {
+				ctx.setTransform(p.s*p.sx, p.s*p.rx, p.s*p.ry, p.s*p.sy, this.s*p.x+this.x, this.s*p.y+this.y)
+				ctx.scale(this.s, this.s)
+				p.r && ctx.rotate(p.r)
+			}
+			for (var m of this.modifiers.render) {
+				m = JPS.TOOLS.TYP(m.type,p.type) && m.render(p,ctx,stime,this,i)
+				if(m) { m == 1 && this.delete(i--); this.skip++; break }
 			}
 		}
 
 		ctx?.restore()
-		this.rendertime = performance.now() - this.rendertime
+		this.rendertime = performance.now() - ctime
 		this.modifiers.post.forEach(m => m.post(ctx,stime,this))
 	}
 }
--- JPS_LIB.js
+++ JPS_LIB.js
@@ -1,67 +1,97 @@
-﻿// JPS - Javascript Particle System  |  v0.9  |  npc299792  |  MIT Licence
+﻿// JPS - Javascript Particle System  |  v0.98  |  npc299792  |  MIT Licence
 
 JPS.LIB ??= {}
 
 // #################################################################################################################################
 
-JPS.TYPES.BASE		= {s:1, r:1, life:1, type:1}
-JPS.TYPES.COLOR		= {r:[128,64], g:[128,64], b:[128,64], a:[0.6,0.4]}
-JPS.TYPES.GRADIENT	= [{i:0.3, r:[200,50], g:[200,50], b:[200,50], a:1}, {i:1, r:0, g:0, b:0, a:0}]
+JPS.TYPES.COLOR			= {r:[128,64], g:[128,64], b:[128,64], a:[0.6,0.4]}
+JPS.TYPES.GRADIENT		= [{i:0.3, r:[200,50], g:[200,50], b:[200,50], a:1}, {i:1, r:0, g:0, b:0, a:.01}]
 
-JPS.TYPES.RECT		= {w:[10,10], h:[10,10], color:JPS.TYPES.COLOR, method:'fillRect'}
-JPS.TYPES.CIRCLE	= {radius:[10,10], color:JPS.TYPES.COLOR, method:'fill'}
-JPS.TYPES.SPHERE	= {radius:[10,10], gradient:JPS.TYPES.GRADIENT}
-JPS.TYPES.TEXT		= {size:[20,10], color:JPS.TYPES.COLOR, font:'Arial', method:'fillText', text:['A','B','C']}
+JPS.TYPES.RECTANGLE		= {w:[10,5], h:[10,5], color:JPS.TYPES.COLOR}
+JPS.TYPES.CIRCLE		= {radius:[10,8], color:JPS.TYPES.COLOR}
+JPS.TYPES.TRIANGLE		= {w:[10,5], h:[10,5], color:JPS.TYPES.COLOR}
+JPS.TYPES.SPHERE		= {radius:[10,5], gradient:JPS.TYPES.GRADIENT}
+JPS.TYPES.TEXT			= {h:[10,5], font:'Arial', text:['A','B','C'], color:JPS.TYPES.COLOR}
 
-JPS.TYPES.EMITTER	= {x:0, y:0, z:0, w:100, h:100, d:0, spawn:0, rate:1}
-JPS.TYPES.ANIMATE	= {id:'v', data:{x:[0,100], y:[0,100]}}
-JPS.TYPES.WALL		= {id:'v', x:0, y:0, w:100, h:100, size:1, bounce:0.6}
-JPS.TYPES.INFO		= {x:20, y:20, size:15, color:'grey', font:'Arial', method:'fillText', text1:'fps: %%fps  /  particles: %%p  /  rendertime: %%rt'}
-JPS.TYPES.CAMERA	= {x:0, y:0, z:-1000, depth:2000, fov:Math.PI/2, center:{x:window.innerWidth/2,y:window.innerHeight/2}, mode:2}
+JPS.TYPES.ALIGN			= {id:'v', offset:0}
+JPS.TYPES.ANIMATE		= {id:'v', x:[0,100], y:[0,100]}
+JPS.TYPES.EMITTER		= {x:[100,100], y:[100,100], z:0, spawn:0, rate:1}
+JPS.TYPES.WALL			= {id:'v', x:0, y:0, w:100, h:100, size:2, bounce:0.6}
+JPS.TYPES.MAGNET		= {id:'', x:-500, y:-500, radius:300, charge:-20, pcharge:[20,10], pow:1}
+JPS.TYPES.CAMERA		= {x:0, y:0, z:-1000, depth:2000, fov:Math.PI/2, center:{x:window.innerWidth/2,y:window.innerHeight/2}, mode:2}
+JPS.TYPES.TWEEN			= {id:'tw', prop:'s', start:0, end:2, starttime:0, endtime:5000, ease:'easein'}
+
+JPS.TYPES.DRAG			= {button:0, dragspeed:1, zoomspeed:.1, pause:'Space'}
+JPS.TYPES.BACKSCREEN	= {w:window.innerWidth, h:window.innerHeight, filter:'opacity(30%) blur(4px)'}
+
+JPS.TYPES.VISIBLE		= {mx:0, my:0, mode:2}
+JPS.TYPES.SLIDER		= {x:10, y:10, w:250, h:25, text:'%%value', start:0, end:100, prop:'x', color1:'white', color2:'grey'}
+JPS.TYPES.INFO			= {x:10, y:20, size:15, color:'grey', font:'Arial', method:'fillText', digits:2, text1:'fps: %%fps  /  particles: %%p  /  rendertime: %%rt'}
 
 // #################################################################################################################################
 
-JPS.TOOLS.RNDASN = function(r, ...a) {
-	for (o of a) for (i in o) r[i] = JPS.TOOLS.RND(o[i])
-	return r
+JPS.TOOLS.RNDASN = function(d, ...a) {
+	var f = []
+	for (o of a) {
+		if (Array.isArray(o)) { var f = o; continue }
+		for (i in o) !f.includes(i) && (d[i] ??= JPS.TOOLS.RND(o[i]))
+	}
+	return d
 }
 
-JPS.TOOLS.RNDBASE = function(a, b) {
-	for (i in JPS.TYPES.BASE) if (b[i]) a[i] = JPS.TOOLS.RND(b[i])
+JPS.TOOLS.RECT = (ctx, x, y, w, h, fill, stroke, size, round) => {
+	if (fill) ctx.fillStyle = fill
+	if (stroke) { ctx.strokeStyle = stroke; ctx.lineWidth = size||1 }
+	if (round) { ctx.beginPath(); ctx.roundRect(x,y,w,h,round) }
+	fill && (round && ctx.fill() || ctx.fillRect(x,y,w,h))
+	stroke && (round && ctx.stroke() || ctx.strokeRect(x,y,w,h))
 }
 
-JPS.TOOLS.PRINT = function(ctx, t, h=20, x=0, y=0, f, c, a, b, m='fillText') {
-	f && (ctx.font = h + 'px ' + f)
-	c && (ctx.fillStyle = c)
-	a && (ctx.textAlign = a)
-	b && (ctx.textBaseline = b)
-	t && ctx[m](t,x,y)
+JPS.TOOLS.CIRC = (ctx, x, y, radius, fill, stroke, size, start = 0, end = 360) => {
+	if (fill) ctx.fillStyle = fill
+	if (stroke) { ctx.strokeStyle = stroke; ctx.lineWidth = size||1 }
+	ctx.beginPath(); ctx.arc(x, y, radius, start, end)
+	fill && ctx.fill(); stroke && ctx.stroke()
 }
 
-JPS.TOOLS.INRECT = (x,y,a,b,c,d) => x > a && x < c && y > b && y < d
+JPS.TOOLS.PRINT = (ctx, text, x=0, y=0, height, color, font, align, base, method='fillText') => {
+	if (color) ctx.fillStyle = color
+	if (align) ctx.textAlign = align
+	if (base)  ctx.textBaseline = base
+	if (height) ctx.font = height + 'px ' + (font && font || '')
+	ctx[method](String(text),x,y)
+}
+
+JPS.TOOLS.EASING = {
+	linear: (v1,v2,r) => (v2-v1)*r+v1,
+	easein: (v1,v2,r,p=2) => (v2-v1)*r**p+v1,
+	easeout: (v1,v2,r,p=2) => -(v2-v1)*r**p*(r-2)+v1,
+	easeinout: (v1,v2,r,p=2) => r<.5 && (v2-v1)*r**p+v1 || -(v2-v1)*r**p*(r-2)+v1
+}
+
+JPS.TOOLS.INRECT = (x,y,a,b,c,d) => x > a && x < a+c && y > b && y < b+d
 JPS.TOOLS.INBOUND = (x,y,w,h,a,b,c,d) => x < a+c && x+w > a && y < b+d && y+h > b
-JPS.TOOLS.INTERSECT = (x,y,u,v,a,b,c,d) => {
-	l = u * d - v * c
+JPS.TOOLS.INTERSECT = (x,y,w,h,a,b,c,d) => {
+	l = w * d - h * c
 	t = -(x * d - y * c - a * d + b * c) / l
 	if (t < 0 || t > 1) return false
-	s = -(x * v - y * u + u * b - v * a) / l
+	s = -(x * h - y * w + w * b - h * a) / l
 	return s > 0 && s < 1
 }
+
 
 // #################################################################################################################################
 
 
 JPS.LIB.RECTANGLE = class {
 	constructor(data) {
-		Object.assign(this, JPS.TYPES.RECT, data)
+		Object.assign(this, JPS.TYPES.RECTANGLE, data)
 	}
 	init(p) {
-		JPS.TOOLS.RNDASN(p, this)
+		JPS.TOOLS.RNDASN(p, ['type'], this)
 	}
 	render(p, ctx) {
-		if (!ctx) return
-		ctx.fillStyle = p.color
-		ctx[this.method](-p.w/2, -p.h/2, p.w, p.h)
+		ctx && JPS.TOOLS.RECT(ctx, -p.w/2, -p.h/2, p.w, p.h, p.color, p.stroke, p.line, p.round)
 	}
 }
 
@@ -71,12 +101,10 @@ JPS.LIB.CIRCLE = class {
 		Object.assign(this, JPS.TYPES.CIRCLE, data)
 	}
 	init(p) {
-		JPS.TOOLS.RNDASN(p, this)
+		JPS.TOOLS.RNDASN(p, ['type'], this)
 	}
 	render(p, ctx) {
-		if (!ctx) return
-		ctx.fillStyle = p.color
-		ctx.beginPath(); ctx.arc(0, 0, p.radius, 0, 360); ctx[this.method]()
+		ctx && JPS.TOOLS.CIRC(ctx, 0, 0, p.radius, p.color, p.stroke, p.line, p.start, p.end)
 	}
 }
 
@@ -86,15 +114,31 @@ JPS.LIB.SPHERE = class {
 		Object.assign(this, JPS.TYPES.SPHERE, data)
 	}
 	init(p, ctx) {
-		JPS.TOOLS.RNDBASE(p, this)
-		p.radius = JPS.TOOLS.RND(this.radius)
+		JPS.TOOLS.RNDASN(p, ['type'], this)
 		p.gradient = ctx.createRadialGradient(0,0,0,0,0,p.radius)
 		this.gradient.forEach(c => p.gradient.addColorStop(c.i, JPS.TOOLS.RND(c)))
 	}
 	render(p, ctx) {
+		ctx && JPS.TOOLS.CIRC(ctx, 0, 0, p.radius, p.gradient, p.stroke, p.line, p.start, p.end)
+	}
+}
+
+
+JPS.LIB.TRIANGLE = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.TRIANGLE, data)
+	}
+	init(p) {
+		JPS.TOOLS.RNDASN(p, ['type'], this)
+	}
+	render(p, ctx) {
 		if (!ctx) return
-		ctx.fillStyle = p.gradient
-		ctx.beginPath(); ctx.arc(0, 0, p.radius, 0, 360); ctx.fill()
+		ctx.fillStyle = p.color
+		ctx.beginPath()
+   		ctx.moveTo(p.w/2, 0)
+    	ctx.lineTo(-p.w/2, p.h/2)
+    	ctx.lineTo(-p.w/2, -p.h/2)
+    	ctx.fill()
 	}
 }
 
@@ -116,15 +160,13 @@ JPS.LIB.TEXT = class {
 		Object.assign(this, JPS.TYPES.TEXT, data)
 	}
 	init(p) {
-		JPS.TOOLS.RNDBASE(p, this)
-		p.h		= JPS.TOOLS.RND(this.size,0,1)
-		p.color	= JPS.TOOLS.RNDRGB(this.color)
-		p.text  = this.text[~~(Math.random()*this.text.length)]
+		JPS.TOOLS.RNDASN(p, ['type'], this)
 	}
 	render(p, ctx) {
-		ctx && JPS.TOOLS.PRINT(ctx, p.text, p.h, 0, p.h, this.font, p.color, 0,0, this.method)
+		ctx && JPS.TOOLS.PRINT(ctx, p.text, 0, 0, p.h, p.color, p.font, 0,0, p.method) //x:p.h?
 	}
 }
+
 
 // #################################################################################################################################
 
@@ -133,15 +175,17 @@ JPS.LIB.EMITTER = class {
 	constructor(data) {
 		Object.assign(this, JPS.TYPES.EMITTER, data)
 	}
-	post(ctx, time, sys) {
+	prev(ctx, time, sys) {
 		this.spawn += time * this.rate
+		var a = {}
 		while (this.spawn >= 1) {
-			var p = sys.spawn(1, ctx)
-			JPS.TOOLS.RNDBASE(p, this)
-			if (Array.isArray(this.type)) p.type = this.type[~~(Math.random()*this.type.length)]
-			p.x = JPS.TOOLS.RND(this.x, this.w/2)
-			p.y = JPS.TOOLS.RND(this.y, this.h/2)
-			p.z = JPS.TOOLS.RND(this.z, this.d/2)
+			a.x = JPS.TOOLS.RND(this.x, this.w && this.w/2)
+			a.y = JPS.TOOLS.RND(this.y, this.h && this.h/2)
+			a.z = JPS.TOOLS.RND(this.z, this.d && this.d/2)
+			if (this.type) a.type = Array.isArray(this.type) && this.type[~~(Math.random()*this.type.length)] || this.type
+			if (this.life) a.life = JPS.TOOLS.RND(this.life)
+
+			sys.spawn(1, ctx, a)
 			this.spawn--
 		}
 	}
@@ -149,12 +193,15 @@ JPS.LIB.EMITTER = class {
 
 
 JPS.LIB.ANIMATE = class {
-	constructor(data, id) {
-		Object.assign(this, JPS.TYPES.ANIMATE, data)
-		this.data = data || this.data
+	id = 'v'
+	constructor(data) {
+		Object.assign(this, data || JPS.TYPES.ANIMATE)
+		this.data = {...this}
+		delete this.data.type
+		delete this.data.id
 	}
 	init(p) {
-		for (var e in this.data) p[this.id+e] = JPS.TOOLS.RND(this.data[e])
+		for (var e in this.data) p[this.id+e] ??= JPS.TOOLS.RND(this.data[e])
 	}
 	render(p, ctx, time) {
 		for (var e in this.data) p[e] += p[this.id+e] * time
@@ -165,11 +212,12 @@ JPS.LIB.ANIMATE = class {
 JPS.LIB.CAMERA = class {
 	constructor(data) {
 		Object.assign(this, JPS.TYPES.CAMERA, data)
+		this.init = p => p.z ??= 0
 	}
 	render(p, ctx) {
 		if (!ctx) return
 		
-		var w = 1 - this.depth / (p.z - this.z)
+		var w = 1 - this.depth / (p.z-this.z)
 		if (w >= 0) return this.mode
 		
 		p.r && ctx.rotate(-p.r)
@@ -186,9 +234,10 @@ JPS.LIB.CAMERA = class {
 JPS.LIB.WALL = class {
 	constructor(data) {
 		Object.assign(this, JPS.TYPES.WALL, data)
-		this.post = this.color && function(ctx) { if (ctx) {
-			ctx.strokeStyle = this.color || 'red'; ctx.lineWidth = this.size
-			ctx.beginPath(); ctx.moveTo(this.x,this.y); ctx.lineTo(this.x+this.w,this.y+this.h); ctx.stroke()
+		this.post = this.color && function(ctx,time,sys) { if (ctx) {
+			ctx.strokeStyle = this.color; ctx.lineWidth = this.size * sys.s
+			ctx.beginPath(); ctx.moveTo(this.x + sys.x, this.y + sys.x); 
+			ctx.lineTo(this.x + sys.x + this.w, this.y + sys.x + this.h); ctx.stroke()
 		}}
 	}
 	render(p, ctx, time, sys) {
@@ -206,17 +255,221 @@ JPS.LIB.WALL = class {
 }
 
 
+JPS.LIB.ALIGN = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.ALIGN, data)
+	}
+	render(p, ctx, time) {
+		if (this.target) p.r = Math.atan2(p.y - this.target.y, p.x - this.target.x) + this.offset
+		else p.r = Math.atan2(p[this.id+'y'], p[this.id+'x']) + this.offset
+	}
+}
+
+
+JPS.LIB.MAGNET = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.MAGNET, data)
+		this.prev = this.target && function() {
+			if (!this.target) return
+			this.x = this.target.x; this.y = this.target.y
+		}
+	}
+	init(p) {
+		p.charge ??= JPS.TOOLS.RND(this.pcharge)
+	}
+	render(p, ctx, time) {
+		var dx = this.x-p.x, dy = this.y-p.y
+		var d = Math.sqrt(dx**2 + dy**2)
+		if (d < this.radius) {
+			var f = (1-d/this.radius) ** this.pow * this.charge * p.charge * time
+			d = Math.abs(dx) + Math.abs(dy)
+			p[this.id+'x'] -= dx/d * f
+			p[this.id+'y'] -= dy/d * f
+		}
+	}
+}
+
+
+JPS.LIB.TWEEN = class {
+	constructor(data) {
+		this.data = Object.assign(this, JPS.TYPES.TWEEN, data)
+		delete this.data.type
+		delete this.data.id
+	}
+	init(p) {
+		var d = JPS.TOOLS.RNDASN({}, this.data)
+		if (d.starttime < 0 && p.life) d.starttime = p.life+d.starttime
+		if (d.endtime < 0 && p.life) d.endtime = p.life+d.endtime
+		d.start ??= p[d.prop]; d.end ??= p[d.end]
+		p[this.id] = d
+	}
+	render(p) {
+		var d = p[this.id]
+		var t = d.endtime - d.starttime
+		var s = p.age < d.starttime && d.left || (p.age > d.endtime && d.right || 0)
+		var v = Math.abs(p.age - d.starttime) % t / t
+		if (p.age < d.starttime) v = 1 - v
+		if (s == 3) {
+			s = (p.age - d.starttime) / t
+			if (~~(s < 0 && s-1 || s) % 2) v = 1 - v
+		}
+		p[d.prop] = JPS.TOOLS.EASING[d.ease](d.start, d.end, v, d.pow)
+	}
+}
+
+
+// #################################################################################################################################
+
+
+JPS.LIB.VISIBLE = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.VISIBLE, data)
+		document.addEventListener('mousemove', this.move.bind(this))
+	}
+	move(e) {
+		this.mx = e.clientX
+		this.my = e.clientY
+	}
+	init(p) {
+		this.render(p)
+	}
+	prev(ctx, time, sys) {
+		this.select = null
+		this.x = (this.mx-sys.x) / sys.s; this.y = (this.my-sys.y) / sys.s
+		if (ctx) { this.w = ctx.canvas.width; this.h = ctx.canvas.height }
+	}
+	render(p,ctx) {
+		var w = (p.w&&p.w/2)||p.radius||20, h = (p.h&&p.h/2)||p.radius||20
+		p.visible = JPS.TOOLS.INBOUND(p.x-w/2, p.y-h/2, w, h, 0, 0, this.w || ctx.canvas.width, this.h || ctx.canvas.width)
+		p.mouseover = JPS.TOOLS.INRECT(this.x, this.y, 0, 0, this.w, this.h)
+		p.mouseover && (this.select = p)
+		return !p.visible && this.mode
+	}
+}
+
+
+JPS.LIB.BACKSCREEN = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.BACKSCREEN, data)
+		this.can = document.createElement('canvas')
+		this.ctx = this.can.getContext('2d')
+		this.can.width = this.w
+		this.can.height = this.h
+		this.ctx.filter = this.filter
+		this.clear = () => this.ctx.clearRect(0, 0, this.w, this.h)
+	}
+	prev(ctx, time) {
+		if (!this.noback) ctx.drawImage(this.can, 0, 0)
+	}
+	post(ctx, time) {
+		this.ctx.drawImage(ctx.canvas, 0, 0)
+		if (this.replace) { ctx.clearRect(0, 0, this.w, this.h); ctx.drawImage(this.can, 0, 0) }
+	}
+}
+
+
+// #################################################################################################################################
+
+
 JPS.LIB.INFO = class {
 	constructor(data) {
 		Object.assign(this, JPS.TYPES.INFO, data)
 		this.render = this.text2 && function(p,ctx) { if (ctx) {
-			for (var i in p) var txt = this.text2.replace('%%'+i, typeof p[i] == Number && p[i].tofixed(2) || p[i])
+			for (var i in p) var txt = this.text2.replace('%%'+i, typeof p[i] == Number && p[i].toFixed(this.digits) || String(p[i]))
 			JPS.TOOLS.PRINT(ctx, txt, this.size, this.x, this.y, this.font, this.color, this.align, this.base, this.method)
 		}}
 	}
 	post(ctx, time, sys) {
-		var txt = this.text1.replace('%%p', sys.particles.length).replace('%%fps', Math.round(1000/sys.delta)).replace('%%rt', sys.rendertime.toFixed(2))
+		var txt = this.text1.replace('%%p', sys.particles.length).replace('%%fps', Math.round(1000/sys.delta)).replace('%%rt', sys.rendertime.toFixed(this.digits))
 		txt = txt.replace('%%ft', time*1000).replace('%%time', ~~sys.time).replace('%%speed', sys.speed).replace('%%skip', sys.skip)
-		JPS.TOOLS.PRINT(ctx, txt, this.size, this.x, this.y, this.font, this.color, this.align, this.base, this.method)
+		JPS.TOOLS.PRINT(ctx, txt, this.x, this.y, this.size, this.color, this.font, this.align, this.base, this.method)
+	}
+}
+
+
+JPS.LIB.DRAG = class {
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.DRAG, data)
+		this.up = e => this.drag = 0
+		this.add = sys => this.sys = sys
+		this.remove = sys => this.sys = null
+		document.addEventListener('keydown', this.key.bind(this))
+		document.addEventListener('mouseup', this.up.bind(this))
+		document.addEventListener('mousedown', this.down.bind(this))
+		document.addEventListener('mousemove', this.move.bind(this))
+		document.addEventListener('wheel', this.wheel.bind(this))
+		if (this.exclude && !Array.isArray(this.exclude)) this.exclude = [this.exclude]
+	}
+	down(e) {
+		if (e.button != this.button || !this.sys) return
+		if (this.exclude) for (var a of this.exclude) if(JPS.TOOLS.INRECT(e.clientX, e.clientY, a.x, a.y, a.w, a.h)) return
+		this.drag = true
+	}
+	move(e) {
+		if (!this.drag || !this.sys) return
+		this.sys.x += e.movementX * this.dragspeed
+		this.sys.y += e.movementY * this.dragspeed
+	}
+	wheel(e) {
+		if (!this.sys) return
+		var s = (e.deltaY < 0 && -1 || 1) * (this.sys.s * this.zoomspeed)
+		this.sys.x += (e.clientX-this.sys.x) / this.sys.s * s
+		this.sys.y += (e.clientY-this.sys.y) / this.sys.s * s
+		this.sys.s -= s
+	}
+	key(e) {
+		if (!this.sys) return
+		if (e.code == this.pause) {
+			this.paused = !this.paused
+			this.paused && (this.pausespeed = this.sys.speed)
+			this.sys.speed = !this.paused && this.pausespeed || 0
+		}
+	}
+}
+
+
+JPS.LIB.SLIDER = class {
+	line = 2; margin = 5; slider = .1; digits = 2; font = 'Arial'
+	constructor(data) {
+		Object.assign(this, JPS.TYPES.SLIDER, data)
+		this.up = () => this.drag = 0
+		this.add = sys => this.target ??= sys
+		document.addEventListener('mouseup', this.up.bind(this))
+		document.addEventListener('mousedown', this.down.bind(this))
+		document.addEventListener('mousemove', this.move.bind(this))
+	}
+	down(e) {
+		if (this.drag || !this.target) return
+		if (!JPS.TOOLS.INRECT(e.clientX, e.clientY, this.x, this.y, this.w, this.h)) return
+		this.drag = true
+		this.move(e)
+	}
+	move(e) {
+		if (!this.drag) return
+		var v = this.vertical && (1 - (e.clientY - this.y) / this.h) || ((e.clientX - this.x) / this.w)
+		this.target[this.prop] = Math.min(this.end, Math.max(this.start, v * (this.end-this.start) + this.start))
+	}
+	post(ctx) {
+		if (!ctx || !this.target) return
+
+		ctx.save()
+		ctx.setTransform(1, 0, 0, 1, this.x, this.y+(this.vertical&&this.h||0))
+		
+		JPS.TOOLS.RECT(ctx, 0, 0, this.w, this.h, this.color2, this.color1, this.line)
+		
+		var v = (this.target[this.prop] || 0)
+		var w = this.w - this.margin*2 - this.w*this.slider
+		var c = Math.min(1, Math.max(0, (v-this.start) / (this.end-this.start)))
+		if (this.vertical) c = 1 - c
+
+		JPS.TOOLS.PRINT(ctx, 
+			this.text.replace('%%value', v.toFixed(this.digits)), 
+			this.margin+(c<.5&&w+this.w*this.slider||0), this.margin, this.h-this.margin*2, 
+			this.color1, this.font, c<.5 && 'right' || 'left', 'top'
+		)
+
+		JPS.TOOLS.RECT(ctx, this.margin+c*w, this.margin, this.w*this.slider, this.h-this.margin*2, this.color1)
+
+		ctx.restore()
 	}
 }
